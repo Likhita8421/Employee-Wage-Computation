47e8182be7998b2360f1969a9fa506cb5fde0a40
HP <leenupandagre24@gmail.com>
2022-05-21 00:38:09 +0530

    initial commit

---
+++ Employee-Wage-Computation/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Constants
+        int Full_Time = 1;
+        Random random = new Random();
+        //Computation
+        int empCheck = random.Next(0, 2);
+        if (empCheck == Full_Time)
+        {
+            Console.WriteLine("Employee is Present");
+
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absent");
+        }
+
+
+    }
+}
