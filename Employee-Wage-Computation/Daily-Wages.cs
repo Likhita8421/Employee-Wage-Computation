6a7b727c5aa5a36046abd17f3d0078ad2d5fff73
HP <leenupandagre24@gmail.com>
2022-05-21 01:09:29 +0530

    UC ADDED

---
+++ Employee-Wage-Computation/Daily-Wages.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //Constants
+        int Full_Time = 1;
+        Random random = new Random();
+        //Computation
+        int empCheck = random.Next(0, 2);
+        if (empCheck == Full_Time)
+        {
+            Console.WriteLine("Employee is Present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is absent");
+        }
+    
+    }
+}
